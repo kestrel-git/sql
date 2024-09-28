e04bf2fde35713d25681db0b1891de7300179b6c
kestrel-git <parusvarius.b@gmail.com>
2024-09-29 03:05:29 +0900

    Add

---
+++ leetcode/585 Investments in 2016.sql
@@ -0,0 +1,19 @@
+WITH UniqueCity AS (
+    SELECT
+        lat, 
+        lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+),
+Tiv2015 AS (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) >= 2
+)
+SELECT ROUND(SUM(tiv_2016)::NUMERIC, 2) AS tiv_2016
+FROM Insurance
+WHERE (lat, lon) IN (SELECT * FROM UniqueCity)
+  AND tiv_2015 IN (SELECT * FROM Tiv2015)
+;
