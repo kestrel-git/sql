231023092298f609712fa5a18743d2dc61995bca
kestrel-git <parusvarius.b@gmail.com>
2024-12-15 00:23:44 +0900

    Add

---
+++ solvesql/최대값을 가진 행 찾기.sql
@@ -0,0 +1,19 @@
+SELECT
+  id
+FROM
+  points
+WHERE
+  x = (
+    SELECT
+      MAX(x)
+    FROM
+      points
+  )
+  OR y = (
+    SELECT
+      MAX(y)
+    FROM
+      points
+  )
+ORDER BY
+  id;
