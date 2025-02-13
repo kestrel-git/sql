a6d5ae49ac3b2c8b707bd461791863e43b32fee7
kestrel <119677019+kestrel-git@users.noreply.github.com>
2025-02-13 17:59:30 +0900

    Update

--- data_analysis_with_sql/03_웹사이트 접속 및 사용 분석/DAU, WAU, MAU (일별 계산).sql
+++ data_analysis_with_sql/03_웹사이트 접속 및 사용 분석/DAU, WAU, MAU (일별 계산).sql
@@ -9,23 +9,23 @@
 -- 1. DAU
 drop table if exists daily_dau;		-- daily_dau 테이블이 이미 존재할 경우 삭제
 
-create table daily_dau as			-- daily_dau 테이블 생성
+create table daily_dau as		-- daily_dau 테이블 생성
 with
-	period_tbl as (  				-- 2016년 8월 2일부터 11월 1일까지 모든 일자 데이터 생성
+	period_tbl as (  		-- 2016년 8월 2일부터 11월 1일까지 모든 일자 데이터 생성
 		select generate_series('2016-08-02'::date, '2016-11-01'::date, '1 day'::interval)::date as dt
 	)
 select
-	p.dt,	   						-- 기준 날짜
+	p.dt,	   			-- 기준 날짜
 	count(distinct user_id) as dau  -- 기준 날짜별 DAU 계산
 from
 	ga.ga_sess as s
-cross join  						-- ga_sess 각 행에 대해 대상 기간 날짜 전체를 갖도록 크로스 조인
+cross join  				-- ga_sess 각 행에 대해 대상 기간 날짜 전체를 갖도록 크로스 조인
 	period_tbl as p
-where  								-- 지난 1일간 기록된 세션 데이터만 필터링
+where  					-- 지난 1일간 기록된 세션 데이터만 필터링
 	p.dt - interval '1 day' <= s.visit_stime 
 	and s.visit_stime < p.dt 
 group by
-	p.dt;							-- 기준 날짜별 그룹화
+	p.dt;				-- 기준 날짜별 그룹화
 
 
 
